7bfd616ff3883447043dc5b1d87608e8569d5041
Дмитрий Бек <kutsia2011@mail.ru>
2018-09-30 17:28:55 +0300

    implement check brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let bracketsPairList = bracketsConfig.map((elem) => elem[0] + elem[1]);
+    let foundedFlag = true;
+
+    while (foundedFlag) {
+        foundedFlag = false;
+
+        bracketsPairList.forEach((elem) => {
+            while (str.indexOf(elem) !== -1) {
+                str = str.replace(elem, '');
+                foundedFlag = true;
+            }
+        });
+    }
+
+    return !str;
 }
